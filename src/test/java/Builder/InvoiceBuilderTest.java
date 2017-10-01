c5c9e77be7b1c9f13d5c681a101d73e833afadb6
User <Adrian>
2017-10-01 21:19:27 +0200

    haciendo el test de builder

--- src/test/java/Builder/InvoiceBuilderTest.java
+++ src/test/java/Builder/InvoiceBuilderTest.java
@@ -12,10 +12,12 @@ import Buider.ShoppingBuilder;
 
 public class InvoiceBuilderTest {
 
+    @SuppressWarnings("deprecation")
     @Test
     public void testConstructor() {
         Invoice invoiceTest = new InvoiceBuilder(1).client("ADRIAN").total(200).date(new Date()).shop(new ShoppingBuilder(1).amount(2).cost(200).build()).build();
         assertEquals("ADRIAN",invoiceTest.getClient());
+        assertEquals(200,invoiceTest.getTotal());
         
     }
 
